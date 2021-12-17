b473cee61e9f891d5028d50fc98c5e620c7ae0f3
usernamenotused <nelson.brandons@gmail.com>
2021-12-17 11:46:41 -0500

    Create scanps.ps1

---
+++ scanps.ps1
@@ -0,0 +1,11 @@
+Add-Type -AssemblyName "system.io.compression.filesystem"
+foreach($Drive in $(Get-PSDrive -PSProvider FileSystem)){ #added ErrorAction Ignore and search hidden files too
+    Get-ChildItem $Drive.Root -Recurse -Include '*.jar', '*.war', '*.ear' -ea ignore -force | ForEach-Object {
+        $fn = $_.FullName
+        $z = [io.compression.zipfile]::OpenRead( $fn )
+        switch -regex ($z.Entries.Name){ # switchout for Entries
+          'log4j-core-2.1[0-4](?:\-alpha\d|\-beta\d|\.\d)\.jar' { Foreach( $item in $_ ) { Add-Content -path C:\temp\log4war.log -value ('log4j Version in archive 2.10<2.15::'+$_ +'::'+ $fn) } }
+          'log4j-core-2.[0-9](?:\-alpha\d|\-bedta\d|\.\d)\.jar' { Foreach( $item in $_ ) {Add-Content -path C:\temp\log4war.log -value ('log4j Version in archive < 2.10 ::'+$_ +'::'+ $fn) } }
+        }
+    }
+}
